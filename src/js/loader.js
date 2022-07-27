92282fa5aaa9a52d306642ff8a2499f4112e6900
clocwerk <65974401+clocwerk@users.noreply.github.com>
2022-07-27 22:05:27 +0300

    1

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,4 +1,11 @@
-window.onload = function () {
-  let preloader = document.querySelector('.loader');
-  preloader.getElementsByClassName.display = 'none ';
-};
+// window.onload = function () {
+//   let preloader = document.querySelector('.loader');
+//   preloader.getElementsByClassName.display = 'none ';
+// };
+// window.onload = function () {
+//   document.body.classList.add('loaded_hiding');
+//   window.setTimeout(function () {
+//     document.body.classList.add('loaded');
+//     document.body.classList.remove('loaded_hiding');
+//   }, 500);
+// };
