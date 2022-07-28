da05fcec7ad6cf59a558140e3d55ecf72bcb66c1
clocwerk <65974401+clocwerk@users.noreply.github.com>
2022-07-28 18:10:13 +0300

    1

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,11 +1,7 @@
-// window.onload = function () {
-//   let preloader = document.querySelector('.loader');
-//   preloader.getElementsByClassName.display = 'none ';
-// };
-// window.onload = function () {
-//   document.body.classList.add('loaded_hiding');
-//   window.setTimeout(function () {
-//     document.body.classList.add('loaded');
-//     document.body.classList.remove('loaded_hiding');
-//   }, 500);
-// };
+window.onload = function () {
+  document.body.classList.add('loaded_hiding');
+  window.setTimeout(function () {
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded_hiding');
+  }, 500);
+};
